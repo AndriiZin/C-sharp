e33d4af29bb4d4c4249efe807bfb319d3eee0bae
AndriiZin <55133708+AndriiZin@users.noreply.github.com>
2019-12-16 18:52:04 +0200

    Add files via upload

---
+++ Lab_11/Conveyer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_11
+{
+    class Conveyer
+    {
+        public Conveyer() { }
+        public void TekeSizes(Product prod)
+        {
+            prod.tekenSizes = true;
+        }
+        public void CutOff(Product prod)
+        {
+            prod.cutOff = true;
+        }
+        public void Sharpen(Product prod)
+        {
+            prod.sharpened = true;
+        }
+        public void CutThrean(Product prod)
+        {
+            prod.cutThreat = true;
+        }
+        public void Drillen(Product prod)
+        {
+            prod.drilled = true;
+        }
+        public void Painten(Product prod)
+        {
+            prod.painted = true;
+        }
+        public void Protest(Product prod)
+        {
+            prod.tested = true;
+        }
+        public void Packen(Product prod)
+        {
+            prod.painted = true;
+        }
+
+
+    }
+}
---
+++ Lab_11/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_11
+{
+    class Program
+    {
+        public delegate void DelegateConveyer(Product prod);
+        static void Main(string[] args)
+        {
+            // 1.
+            Product prod = new Product();
+            prod.OutputInfo();
+            Conveyer exConv = new Conveyer();
+            DelegateConveyer conveyer = exConv.TekeSizes;
+            conveyer += exConv.CutOff;
+            conveyer += exConv.Sharpen;
+            conveyer += exConv.CutThrean;
+            conveyer += exConv.Drillen;
+            conveyer += exConv.Painten;
+            conveyer += exConv.Protest;
+            conveyer += exConv.Packen;
+            conveyer(prod);
+            prod.OutputInfo();
+            Console.ReadKey();
+        }
+    }
+}
